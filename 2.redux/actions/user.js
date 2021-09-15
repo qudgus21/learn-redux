263df24001fc558af2e3c984a57c2cfe6263fcf3
qudgus21 <qudgus54321@naver.com>
2021-09-15 20:09:45 +0900

    파일 분리

---
+++ 2.redux/actions/user.js
@@ -0,0 +1,16 @@
+const logIn = (data) => {
+  return {
+    // action
+    type: "LOG_IN",
+    data,
+  };
+};
+
+const logOut = () => {
+  return {
+    // action
+    type: "LOG_OUT",
+  };
+};
+
+export { logIn, logOut };
