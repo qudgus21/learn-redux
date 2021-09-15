f0f32be79336fc971073b3ae3e13de8ac888b92e
qudgus21 <qudgus54321@naver.com>
2021-09-16 08:57:00 +0900

    redux 테스트를 위한 cra 세팅

---
+++ 3.redux/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>redux test start</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
