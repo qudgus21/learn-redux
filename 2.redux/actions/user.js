dba2e661696f8ad91d283dc4b7e6ac03b749239f
qudgus21 <qudgus54321@naver.com>
2021-09-16 08:37:20 +0900

    redux thunk 적용

--- 2.redux/actions/user.js
+++ 2.redux/actions/user.js
@@ -1,10 +1,36 @@
+//async action creator
+
 const logIn = (data) => {
-  return {
-    // action
-    type: "LOG_IN",
-    data,
+  //action
+  return (dispatch, getState) => {
+    // 요청이 시작했음을 알리는 액션
+    dispatch({ type: "LOG_IN_REQUEST" });
+
+    loginRequest(data)
+      .then((info) => {
+        dispatch({ type: "LOG_IN_SUCCESS", data });
+      })
+      .catch((e) => dispatch({ type: "LOG_IN_Fail", error: e }));
   };
 };
+
+const loginRequest = (data) => {
+  return new Promise((resolve) => {
+    resolve({
+      user: data,
+      logined: true,
+    });
+  });
+};
+
+//sync action creator
+// const logIn = (data) => {
+//   return {
+//     // action
+//     type: "LOG_IN",
+//     data,
+//   };
+// };
 
 const logOut = () => {
   return {
--- 2.redux/index.js
+++ 2.redux/index.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { logIn, logOut } from "./actions/user.js";
 import { addPost } from "./actions/post.js";
 import reducer from "./reducers/index.js";
@@ -16,7 +16,37 @@ const initialState = {
   // followers: [],
 };
 
-const store = createStore(reducer, initialState);
+// function firstMiddleware(store){
+//   return function(next){
+//     return function(action){
+
+//     }
+//   }
+// }
+const firstMiddleware = (store) => (next) => (action) => {
+  console.log("action start...", action);
+  next(action);
+};
+
+const thunkMiddleware = (store) => (next) => (action) => {
+  if (typeof action === "function") {
+    return action(store.dispatch, store.getState);
+  }
+  return next(action);
+};
+
+const enhancer = applyMiddleware(firstMiddleware, thunkMiddleware);
+
+// middleware 여러개 사용할 경우 이 방식으로도 가능
+// const enhancer = compose(
+//   applyMiddleware(),
+// )
+
+const store = createStore(reducer, initialState, enhancer);
+
+// store.subscribe(() => {
+//   console.log("subscribe");
+// });
 
 console.log("1nd", store.getState());
 
@@ -30,16 +60,16 @@ store.dispatch(
 
 console.log("2nd", store.getState());
 
-store.dispatch(
-  addPost({
-    userId: 1,
-    id: 1,
-    content: "안녕하세요. 리덕스",
-  })
-);
+// store.dispatch(
+//   addPost({
+//     userId: 1,
+//     id: 1,
+//     content: "안녕하세요. 리덕스",
+//   })
+// );
 
-console.log("3rd", store.getState());
+// console.log("3rd", store.getState());
 
-store.dispatch(logOut());
+// store.dispatch(logOut());
 
-console.log("4th", store.getState());
+// console.log("4th", store.getState());
