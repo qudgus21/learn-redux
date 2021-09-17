76b8a4a288a846f451c867094637505caf31b80f
qudgus21 <qudgus54321@naver.com>
2021-09-17 11:49:01 +0900

    immer 적용

--- 4.redux/src/reducers/post.js
+++ 4.redux/src/reducers/post.js
@@ -1,12 +1,17 @@
+import produce from "immer";
+
 const initialState = [];
 
 const postReducer = (prevState = initialState, action) => {
-  switch (action.type) {
-    case "ADD_POST":
-      return [...prevState, action.data];
-    default:
-      return prevState;
-  }
+  return produce(prevState, (draft) => {
+    switch (action.type) {
+      case "ADD_POST":
+        draft.push(action.data);
+        break;
+      default:
+        break;
+    }
+  });
 };
 
 export default postReducer;
--- 4.redux/src/reducers/user.js
+++ 4.redux/src/reducers/user.js
@@ -1,36 +1,33 @@
+import produce from "immer";
+
 const initialState = {
   isLoggingIn: false,
   data: null,
 };
 
+//nextState = produce(prevState , (draft)=>{})
 const userReducer = (prevState = initialState, action) => {
-  switch (action.type) {
-    case "LOG_IN_REQUEST":
-      return {
-        ...prevState,
-        isLoggingIn: true,
-      };
-    case "LOG_IN_SUCCESS":
-      return {
-        ...prevState,
-        isLoggingIn: false,
-        data: action.data,
-      };
-    case "LOG_OUT":
-      return {
-        ...prevState,
-        isLoggingIn: false,
-        data: null,
-      };
-    case "LOG_IN_Fail":
-      return {
-        ...prevState,
-        isLoggingIn: false,
-        data: null,
-      };
-    default:
-      return prevState;
-  }
+  return produce(prevState, (draft) => {
+    switch (action.type) {
+      case "LOG_IN_REQUEST":
+        draft.isLoggingIn = true;
+        break;
+      case "LOG_IN_SUCCESS":
+        draft.isLoggingIn = false;
+        draft.data = action.data;
+        break;
+      case "LOG_OUT":
+        draft.isLoggingIn = false;
+        draft.data = null;
+        break;
+      case "LOG_IN_Fail":
+        draft.isLoggingIn = false;
+        draft.data = null;
+        break;
+      default:
+        break;
+    }
+  });
 };
 
 export default userReducer;
